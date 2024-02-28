d1bcaa299f2740e940e55e92aca7fdf918318f03
李浩然 <lihaoran2048@qq.com>
2024-02-28 21:08:15 +0800

    update

---
+++ realtime-common/src/main/java/com/haoran/gmall/realtime/common/function/DimJoinFunction.java
@@ -0,0 +1,10 @@
+package com.haoran.gmall.realtime.common.function;
+
+import com.alibaba.fastjson.JSONObject;
+
+
+public interface DimJoinFunction<T> {
+    public  String getId( T input);
+    public  String getTableName();
+    public  void join(T input, JSONObject dim);
+}
